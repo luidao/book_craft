46bbed4cb7c21c0997109019309c4eff317e5317
Luigi Klimuk <luigiklimuk@Luigis-MacBook-Pro.local>
2019-08-21 18:09:45 -0300

    first try

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Produto;
+use App\Categoria;
+
+class ProdutosController extends Controller
+{
+    public function listarProdutos(){
+        $produtos = Produto::orderBy('nome')->paginate(6);
+        $categorias = Categoria::all();
+
+        return view('produtos', compact('produtos', 'categorias'));
+    }
+
+    public function detalheProduto($id){
+        $produto = Produto::find($id);  
+
+        return view('produto', compact('produto'));
+    }
+
+    public function filtrarCategoria($id){
+        $nomeCategoria = Categoria::find($id)->nome; // SELECT nome FROM categoria WHERE id = $id
+        $categorias = Categoria::all();
+        $produtos = Produto::where('fk_categoria', '=', $id)->paginate(6);
+
+        return view('produtos', compact('nomeCategoria', 'produtos', 'categorias'));
+    }
+}
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.master')
+
+@section('title', 'Produtos')
+
+@section('content')
+<section id="produtos" class="new-products mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h2>categorias</h2>
+
+                <ul class="categorias">
+                    <li><a href="/produtos">Todos os produtos</a></li>
+                    @if(isset($categorias))
+                        @foreach($categorias as $categoria)
+                            <li><a href="/produtos/categoria/{{ $categoria->id_categoria }}">{{ $categoria->nome }}</a></li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
+            <div class="col-sm-8">
+                <h1>{{ isset($nomeCategoria) ? $nomeCategoria : 'Todos os produtos' }}</h1>
+
+                <div class="row">
+                    @if(isset($produtos) && count($produtos) > 0)
+
+                    @foreach($produtos as $produto)
+                    <div class="livro col-sm-12 col-lg-4 my-4">
+                        <div class="card">
+                            <img class="card-img-top" src="{{ $produto['imagem'] }}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">{{ $produto['nome'] }}</h5>
+                                <ul class="list-group list-group-flush text-center">
+                                    <li class="list-group-item"><strong>R$ {{ $produto['preco'] }}</strong></li>
+                                </ul>
+                                <a href="/produtos/{{ $produto->id_produto }}" class="d-flex btn btn-primary my-3 justify-content-center comprar">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                    <div class="col-lg-12">
+                        {{ $produtos->links() }}
+                    </div>
+                    @else
+
+                    <div class="col text-center">
+                        <span class="alert alert-info">Nenhum produto para exibir</span>
+                    </div>
+
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@stop
